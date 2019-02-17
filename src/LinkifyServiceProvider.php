ead56ee1ef873762264734be826633f6d0392936
Sam Taylor <sam@taylornetwork.ca>
2019-02-17 23:47:07 +0000

    Apply fixes from StyleCI

--- src/LinkifyServiceProvider.php
+++ src/LinkifyServiceProvider.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace TaylorNetwork\Linkify;
 
@@ -6,17 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class LinkifyServiceProvider extends ServiceProvider
 {
-	public function boot()
-	{
-		$this->publishes([
+    public function boot()
+    {
+        $this->publishes([
             __DIR__.'/config/linkify.php' => config_path('linkify.php'),
         ]);
-	}
+    }
 
-	public function register()
-	{
-		$this->mergeConfigFrom(
+    public function register()
+    {
+        $this->mergeConfigFrom(
             __DIR__.'/config/linkify.php', 'linkify'
         );
-	}
+    }
 }
